b5990ee8efbe247ae6ae70daf023eca61344dd01
jastadudley <jastamdudley@gmail.com>
2025-03-12 20:37:00 -0700

    adjusted footer so 'last modified on' actually show a date and time. adjusting some visual aspects to more acuretly copy the lessone example given.

--- W01/scripts/getdates.js
+++ W01/scripts/getdates.js
@@ -1,6 +1,7 @@
+
 const yearSpan = document.querySelector("#currentyear");
 const currentYear = new Date().getFullYear();
 yearSpan.textContent = currentYear;
 
-const lastMod = document.querySelector('#lastModified');
-lastMod.textContent = document.lastModified;
+const lastMod = document.querySelector('#lastmodified');
+lastMod.textContent = "Last Modified: " + document.lastModified;
