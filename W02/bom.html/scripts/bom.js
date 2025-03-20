bbd47fc80db762fb00369ddc18efbdaa19550472
jastadudley <jastamdudley@gmail.com>
2025-03-19 20:09:10 -0700

    adding JS

--- W02/bom.html/scripts/bom.js
+++ W02/bom.html/scripts/bom.js
@@ -3,12 +3,24 @@ const button = document.querySelector('button');
 const list = document.querySelector('#list');
 
 button.addEventListener('click', function() {
+    if (input.value.trim() ===""){
+        input.focus();
+        return;
+    }
+
     const li = document.createElement('li');
     const deleteButton = document.createElement('button');
 
     li.textContent = input.value;
-    deleteButton.textContent = 'X';
+    deleteButton.textContent = '❌';
 
     li.append(deleteButton);
     list.append(li);
+
+    deleteButton.addEventListener('click', function(){
+        list.removeChild(li);
+    })
+
+    input.value = ' ';
+    input.focus();
 });
