2e6a838dce536dd46b7141dea302a516f0e981e6
jastadudley <jastamdudley@gmail.com>
2025-04-06 21:04:41 -0700

    Updating JS for assignment.

--- W05/scripts/bom.js
+++ W05/scripts/bom.js
@@ -2,25 +2,53 @@ const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
 
-button.addEventListener('click', function() {
-    if (input.value.trim() ===""){
+let chaptersArray = getChapterList() || [];
+
+chaptersArray.forEach(chapter => {
+    displayList(chapter);
+});
+
+button.addEventListener('click', function () {
+    if (input.value.trim() === "") {
         input.focus();
         return;
     }
 
-    const li = document.createElement('li');
-    const deleteButton = document.createElement('button');
-
-    li.textContent = input.value;
-    deleteButton.textContent = '❌';
-
-    li.append(deleteButton);
-    list.append(li);
-
-    deleteButton.addEventListener('click', function(){
-        list.removeChild(li);
-    })
-
-    input.value = ' ';
+    displayList(input.value);
+    chaptersArray.push(input.value);
+    setChapterList();
+    input.value = '';
     input.focus();
 });
+
+function displayList(item) {
+    const li = document.createElement('li');
+    li.textContent = item;
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = '❌';
+    deleteBtn.classList.add('delete');
+
+    deleteBtn.addEventListener('click', function () {
+        list.removeChild(li);
+        deleteChapter(li.textContent);
+        input.focus();
+    });
+
+    li.appendChild(deleteBtn);
+    list.appendChild(li);
+}
+
+function setChapterList() {
+    localStorage.setItem('myFavBOMList', JSON.stringify(chaptersArray));
+}
+
+function getChapterList() {
+    return JSON.parse(localStorage.getItem('myFavBOMList'));
+}
+
+function deleteChapter(chapter) {
+    chapter = chapter.slice(0, chapter.length - 1);
+    chaptersArray = chaptersArray.filter(item => item !== chapter);
+    setChapterList();
+}
