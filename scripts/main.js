d8684ab6156137d05c7b72a919a6a0998f99a9c6
jastadudley <jastamdudley@gmail.com>
2025-04-13 21:09:07 -0700

    Adding welcome message.

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,22 @@
 document.getElementById("year").textContent = new Date().getFullYear();
 document.getElementById("lastModified").textContent = `Last Modified: ${document.lastModified}`;
+
+let visitCount = localStorage.getItem("visitCount");
+
+if (visitCount) {
+  visitCount = Number(visitCount) + 1;
+} else {
+  visitCount = 1;
+}
+
+localStorage.setItem("visitCount", visitCount);
+
+
+const welcomeMessages = [
+  "Welcome back!",
+  "So glad you're here again! 💖",
+  "Thanks for visiting us again! 🏡"
+];
+
+let messageIndex = (visitCount - 1) % welcomeMessages.length;
+console.log(`${welcomeMessages[messageIndex]} This is visit #${visitCount}.`);
